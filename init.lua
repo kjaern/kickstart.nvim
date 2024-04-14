e66f00790e2fb4b670d050d3e7c007b9624e1c86
kjaern <kenneth_kjaer_nielsen@hotmail.com>
2024-04-14 13:30:39 +0200

    Added venv selector for python enviroments

--- init.lua
+++ init.lua
@@ -927,6 +927,7 @@ require('lazy').setup {
   --  Here are some example plugins that I've included in the kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
+  require 'kickstart.plugins.python_venv',
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kjaern.remap',
