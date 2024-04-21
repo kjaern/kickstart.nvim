52fbf8f2fbd87e9be835a79cb559af6fe535f998
kknielsen <kknielsen@xnovotech.com>
2024-04-21 20:49:07 +0200

    Added bunch of plugins
    Git filetree ext

--- lua/kickstart/plugins/python_venv.lua
+++ lua/kickstart/plugins/python_venv.lua
@@ -1,10 +1,19 @@
 return {
-  'linux-cultist/venv-selector.nvim',
+  -- 'linux-cultist/venv-selector.nvim',
+  'kjaern/venv-selector.nvim',
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
   opts = {
     -- Your options go here
-    -- name = "venv",
+    name = { 'python', 'conda' },
     -- auto_refresh = false
+    parents = 0,
+    anaconda = {
+      python_executable = nil,
+      python_parent_dir = '',
+      dap_enabled = true,
+      enable_debug_output = true,
+    },
+    python_parent_dir = 'Scripts',
   },
   event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
@@ -13,4 +22,10 @@ return {
     -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
     { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
   },
+  -- config = function()
+  --   require('venv-selector').setup {
+  --
+  --     changed_venv_hooks = { require('venv-selector').hooks.pyright },
+  --   }
+  -- end,
 }
